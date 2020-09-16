73be9930d6b237b8c574f4e67d1f3ee5941cecf1
chaycesol <chayce.solchaga@gmail.com>
2020-09-16 12:20:24 -0700

    Tests for repair and success completed

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,59 @@ module.exports = {
   get,
 };
 
+// let item = {
+//   name: "Sword",
+//   enhancement: "15",
+//   durability: "80"
+// };
+
 function success(item) {
   return { ...item };
+  // accepts item and returns new item modified 
+  //- The item's enhancement increases by 1.
+// - If the item enhancement level is 20, the enhancement level is not changed.
+// - The durability of the item is not changed.
 }
 
 function fail(item) {
-  return { ...item };
+
+  //accepts item and returns new item obj modified by rules
+  if(!item.enhancement) {
+    return { message: 'item unable to be enhanced'}
+  } else if(item.enhancement < 15) {
+    // - If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+    return {...item, durability: item.durability - 5};
+  } else if (item.enhancement === 20) {
+    return item; 
+  } else {
+    // - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+    if(item.enhancement > 16) {
+      return{
+        ...item,
+        durabiilty: item.durability -10,
+        enhancement: item.enhancement - 1, 
+      };
+    }
+      // - If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+    return {...item, durability: item.durability - 10 };
+  }
+
 }
 
 function repair(item) {
-  return { ...item };
+
+  // accepts item and returns new item with XP 100 again
+  if(!item.durability) {
+    return { message: "your item has no durability!"}
+  } else if (item.durability >=100 ) {
+    return { message: "your item's durability is FULL"}
+  } else if (item.durability <= 0) {
+    return { message: "your item is broken"}
+  }
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
+  //**stretch
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,95 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("Item Enhancers", function() {
+    describe("item repair", function () {
+        it("should repair an actual item", function () {
+          const item = {
+            name: "fist weapon",
+            durability: 50,
+            enhancement: 15,
+          };
+          const expected = {
+            name: "fist weapon",
+            durability: 100,
+            enhancement: 15,
+          };
+          const actual = enhancer.repair(item);
+          expect(actual).toStrictEqual(expected);
+        });
+        it("should return a message if no durability found", function() {
+            const item = {
+                name: "Sword",
+                enhancement: 15
+            }
+            const expected = { message: "your item has no durability!"}
+            const actual = enhancer.repair(item)
+            expect(actual).toStrictEqual(expected)
+        });
+        it("should return a message you item has max durability", function() {
+            const item = {
+                name: "Mace",
+                durability: 100,
+                enhancement: 15
+            }
+            const expected = { message: "your item's durability is FULL"}
+            const actual = enhancer.repair(item)
+            expect(actual).toStrictEqual(expected)
+        })
+        it('should return a message if durability is less than 0', function() {
+            const item = {
+                name: "polearm",
+                durability: -5,
+                enhancement: 15
+            }
+            const expected = { message: "your item is broken"}
+            const actual = enhancer.repair(item)
+            expect(actual).toStrictEqual(expected)
+        })
+      });
+      describe('item success', function() {
+          it.todo("The item's enhancement increases by 1.");
+          it.todo("If the item enhancement level is 20, the enhancement level is not changed.");
+          it.todo("The durability of the item is not changed.");
+      });
+      describe('item fail', function() {
+        it("should let the user know that enhancements aren't available on this weapon", function() {
+            const item = {
+                name: "sword",
+                durability: 80
+            }
+            const expected = { message: 'item unable to be enhanced'}
+            const actual = enhancer.fail(item)
+            expect(actual).toStrictEqual(expected)
+        });
+        it('should decrease the durability of an item by 10 if the enhancement level is greater than or equal to 15', function() {
+            const item = {
+                name: "sword",
+                durability: 80,
+                enhancement: 15
+            }
+            const expected =  {
+                name: "sword",
+                durability: 70,
+                enhancement: 15
+            }
+            const actual = enhancer.fail(item)
+            expect(actual).toStrictEqual(expected)
+        });
+        it("should reduce item's durability by 5 if enhancement level is less than 15", function(){
+            const item = {
+                name: "sword",
+                durability: 70,
+                enhancement: 14
+            }
+            const expected = {
+                name: "sword",
+                durability: 65,
+                enhancement: 14
+            }
+            const actual = enhancer.fail(item)
+            expect(actual).toStrictEqual(expected)
+        })
+        it.todo("If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).");
+    });
+})
